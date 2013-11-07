32bc8a4be8c1f631f6e0de6f1a74640822421b82
D4rch4ngel-PC <d4rch4ngel@ubuntu.(none)>
2013-11-06 18:10:24 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGrades::Application.config.secret_token = 'a61a74e8e3b19b47eb5f8cabfe35e66fee538c3eb553fb7d700a8667c5192daa903c1cc01ffbf540790b1ef63ddbe38e535b6ae881f7075042b070788e3a0ba1'
